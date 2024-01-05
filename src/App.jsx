6b1013185e03264f949b76a1075a6cd504df791c
Alin <alin.aldescu@canarytech.ro>
2024-01-06 01:18:23 +0200

    add global styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+
+import GlobalStyles from "./styles/GlobalStyles.js";
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    background-color: var(--color-brand-500);
+`;
+
+const Button = styled.button`
+    font-size: 1.4rem;
+    color: var(--color-indigo-700);
+`;
+
+const StyledApp = styled.main`
+    background-color: orangered;
+    padding: 20px;
+`;
+
 function App() {
-  return <>hi</>;
+    return (
+        <>
+            <GlobalStyles />
+            <StyledApp>
+                <H1>The Wild Oasis</H1>
+                <Button>efew fwe fw</Button>
+            </StyledApp>
+        </>
+    );
 }
 
 export default App;
