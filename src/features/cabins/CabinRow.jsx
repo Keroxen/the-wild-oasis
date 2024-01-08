707f0703184984e29871b87617d843696182cf7a
Alin <alin.aldescu@canarytech.ro>
2024-01-09 00:05:39 +0200

    display toast notifications

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { formatCurrency } from "../../utils/helpers.js";
 import { deleteCabin } from "../../services/apiCabins.js";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
     display: grid;
@@ -51,12 +52,12 @@ const CabinRow = ({ cabin }) => {
     const { isPending: isDeleting, mutate } = useMutation({
         mutationFn: deleteCabin,
         onSuccess: () => {
-            alert("Cabin successfully deleted");
+            toast.success("Cabin successfully deleted");
             queryClient.invalidateQueries({
                 queryKey: ["cabin"]
             })
         },
-        onError: err => alert(err.message)
+        onError: err => toast.error(err.message)
     })
 
     return (
