34fed01e139a152b83f55e0f61b1c5ef6225c3cc
Alin <alin.aldescu@canarytech.ro>
2024-01-12 23:56:31 +0200

    open create cabin form in a modal

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import Button from "../../ui/Button.jsx";
+import Modal from "../../ui/Modal.jsx";
+import CreateCabinForm from "./CreateCabinForm.jsx";
+
+const AddCabin = () => {
+    const [isOpenModal, setIsOpenModal] = useState(false);
+
+    return (
+        <div>
+            <Button onClick={() => setIsOpenModal(!isOpenModal)}>Add new cabin</Button>
+            {isOpenModal && <Modal onClose={() => setIsOpenModal(false)}>
+                <CreateCabinForm />
+            </Modal>}
+        </div>
+    );
+};
+
+export default AddCabin;
