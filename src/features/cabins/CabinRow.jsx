92d9623237eefd11c78f9656007484f6faa1d144
Alin <alin.aldescu@canarytech.ro>
2024-01-13 22:16:08 +0200

    build a reusable table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,18 +8,19 @@ import { useDeleteCabin } from "./useDeleteCabin.js";
 import { useCreateCabin } from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
+import Table from "../../ui/Table.jsx";
 
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
-
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
+// const TableRow = styled.div`
+//     display: grid;
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//     column-gap: 2.4rem;
+//     align-items: center;
+//     padding: 1.4rem 2.4rem;
+//
+//     &:not(:last-child) {
+//         border-bottom: 1px solid var(--color-grey-100);
+//     }
+// `;
 
 const Img = styled.img`
     display: block;
@@ -63,7 +64,7 @@ const CabinRow = ({ cabin }) => {
     }
 
     return (
-        <TableRow role={"row"}>
+        <Table.Row>
             <Img src={image} alt={name} />
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity} guests</div>
@@ -90,7 +91,7 @@ const CabinRow = ({ cabin }) => {
                 </Modal>
 
             </div>
-        </TableRow>
+        </Table.Row>
     );
 };
 
