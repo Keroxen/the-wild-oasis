30b4e5326eb9a89d708edcf95baf8482d957e1e9
Alin <alin.aldescu@canarytech.ro>
2024-01-14 20:47:58 +0200

    add filters

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { useSearchParams } from "react-router-dom";
 
 import Spinner from "../../ui/Spinner.jsx";
 import CabinRow from "./CabinRow.jsx";
@@ -6,26 +6,25 @@ import { useCabins } from "./useCabins.js";
 import Table from "../../ui/Table.jsx";
 import Menus from "../../ui/Menus.jsx";
 
-const TableHeader = styled.header`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-
-    background-color: var(--color-grey-50);
-    border-bottom: 1px solid var(--color-grey-100);
-    text-transform: uppercase;
-    letter-spacing: 0.4px;
-    font-weight: 600;
-    color: var(--color-grey-600);
-    padding: 1.6rem 2.4rem;
-`;
-
 const CabinTable = () => {
     const { isLoading, cabins } = useCabins();
+    const [searchParams] = useSearchParams();
 
     if (isLoading) {
         return <Spinner />
+    }
+
+    const filterValue = searchParams.get("discount") || "all";
+    let filteredCabins;
+
+    if (filterValue === "all") {
+        filteredCabins = cabins;
+    }
+    if (filterValue === "no-discount") {
+        filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+    }
+    if (filterValue === "with-discount") {
+        filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
     }
 
     return (
@@ -40,7 +39,7 @@ const CabinTable = () => {
                     <div></div>
                 </Table.Header>
 
-                <Table.Body data={cabins} render={(cabin) => (
+                <Table.Body data={filteredCabins} render={(cabin) => (
                     <CabinRow cabin={cabin} key={cabin.id} />
                 )} />
             </Table>
---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,25 @@
+import TableOperations from "../../ui/TableOperations.jsx";
+import Filter from "../../ui/Filter.jsx";
+
+const CabinTableOperations = () => {
+    return (
+        <TableOperations>
+            <Filter filterField={"discount"} options={[
+                {
+                    value: "all",
+                    label: "All"
+                },
+                {
+                    value: "no-discount",
+                    label: "No discount"
+                },
+                {
+                    value: "with-discount",
+                    label: "With discount"
+                }
+            ]} />
+        </TableOperations>
+    );
+};
+
+export default CabinTableOperations;
