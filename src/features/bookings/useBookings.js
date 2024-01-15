8bead286d4a654f563909de0a6fde4518a3709c7
Alin <alin.aldescu@canarytech.ro>
2024-01-16 00:37:39 +0200

    add status filtering

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,11 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
 import { getBookings } from "../../services/apiBookings.js";
 
 export function useBookings() {
+    const [searchParams] = useSearchParams();
+
+    // FILTER
+    const filterValue = searchParams.get("status");
+    const filter = !filterValue || filterValue === "all" ? null : {
+        field: "status",
+        value: filterValue
+    };
+
     const { isLoading, data: bookings, error } = useQuery({
-        queryKey: ['bookings'],
-        queryFn: getBookings
+        queryKey: ['bookings', filter], // fetches the data when filter changes, like a dep array
+        queryFn: () => getBookings({ filter })
     })
 
     return { isLoading, bookings, error }
