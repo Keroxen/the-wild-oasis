5b46594508cc97e2a83bf2de9a414e0c705c8763
Alin <alin.aldescu@canarytech.ro>
2024-01-19 01:12:29 +0200

    create logout API

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,17 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+
+import ButtonIcon from "../../ui/ButtonIcon.jsx";
+import { useLogout } from "./useLogout.js";
+import SpinnerMini from "../../ui/SpinnerMini.jsx";
+
+const Logout = () => {
+    const { logout, isPending } = useLogout();
+
+    return (
+        <ButtonIcon disabled={isPending} onClick={logout}>
+            {!isPending ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+        </ButtonIcon>
+    );
+};
+
+export default Logout;
