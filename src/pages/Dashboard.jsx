f31c162a9a6c6461b134669320d97a6af1434869
Alin <alin.aldescu@canarytech.ro>
2024-01-23 00:58:42 +0200

    calculate dashboard stats

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,20 +2,8 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import DashboardLayout from "../features/dashboard/DashboardLayout.jsx";
 import DashboardFilter from "../features/dashboard/DashboardFilter.jsx";
-import { useRecentBookings } from "../features/dashboard/useRecentBookings.js";
-import Spinner from "../ui/Spinner.jsx";
-import { useRecentStays } from "../features/dashboard/useRecentStays.js";
 
 function Dashboard() {
-    const { bookings, isLoading: isLoading1 } = useRecentBookings();
-    const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
-
-    if (isLoading1 || isLoading2) {
-        return <Spinner />;
-    }
-
-    console.log(bookings);
-
     return (
         <>
             <Row type="horizontal">
