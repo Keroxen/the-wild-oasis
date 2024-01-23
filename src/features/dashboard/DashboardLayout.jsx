0ce207a3f1c35afb7dabe456ad581f65f49f41a4
Alin <alin.aldescu@canarytech.ro>
2024-01-23 23:23:58 +0200

    create stays pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import { useRecentStays } from "./useRecentStays.js";
 import Spinner from "../../ui/Spinner.jsx";
 import { useCabins } from "../cabins/useCabins.js";
 import SalesChart from "./SalesChart.jsx";
+import DurationChart from "./DurationChart.jsx";
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -28,7 +29,7 @@ const DashboardLayout = () => {
             <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCount={cabins.length} />
             <div>Statistics</div>
             <div>Today&apos;s activity</div>
-            <div>Chart stay durations</div>
+            <DurationChart confirmedStays={confirmedStays} />
             <SalesChart bookings={bookings} numDays={numDays} />
         </StyledDashboardLayout>
     );
